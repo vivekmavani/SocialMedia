832e1f45323472170d35a5e61336ad73ff027bfa
Romish Patoriya <patoriyaromish@gmail.com>
2021-08-23 18:45:32 +0530

    changes quries

--- social.sql
+++ social.sql
@@ -325,7 +325,7 @@ INSERT INTO FriendAccept VALUES (1,3),
 
 --DISPLAY FRIEND
 
-SELECT U.Uid,U.Name FROM FriendAccapt FA JOIN Users U ON FA.Uid = U.Uid WHERE FA.Frid = 3
+SELECT U.Uid,U.Name FROM FriendAccept FA JOIN Users U ON FA.Uid = U.Uid WHERE FA.Frid = 3
 
 --UN FRIEND
 DECLARE @UNFriendAccid INT
@@ -371,10 +371,10 @@ DELETE FROM FriendRequest WHERE Frid_r = 1  AND Uid_s  = 1
 --  acceapte request
 
 
-INSERT INTO FriendAccapt VALUES ((SELECT Uid_s FROM FriendRequest WHERE FriendRequestid = 6),
+INSERT INTO FriendAccept VALUES ((SELECT Uid_s FROM FriendRequest WHERE FriendRequestid = 6),
                                   (SELECT Frid_r FROM FriendRequest WHERE FriendRequestid = 6)) 
 DELETE FROM FriendRequest WHERE FriendRequestid = 6
-SELECT * FROM FriendAccapt 
+SELECT * FROM FriendAccept 
 SELECT * FROM FriendRequest
 
 
