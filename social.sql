763306c4fd329136da58afb4ab2b19e01e68fde2
vivekmavani <vivekcmavani@gmail.com>
2021-08-25 13:56:46 +0530

    update likes query

--- social.sql
+++ social.sql
@@ -666,6 +666,7 @@ SELECT Pid,Title,Likes,Post_Date FROM Post  WHERE Post_Date = CONVERT(DATE,GETDA
 
 Select * FROM Likebyuser
 
+
 --add like by users
 DECLARE @POSTID INT
 SET @POSTID = 4
@@ -683,23 +684,63 @@ SELECT * FROM Post WHERE Pid IN(SELECT LikebyUser_Pid FROM Likebyuser WHERE Like
 -- who likes the post 
 SELECT a.Name,a.Uid FROM Users a JOIN Likebyuser b ON a.Uid = b.LikebyUser_Uid WHERE LikebyUser_Pid = 3
 
-
- 
--- display post by your friends likes
+-- display your post like by  your friends .
    SELECT a.Pid,a.Title,a.Description,c.Image,a.Likes,b.Category_Name,a.Post_Date,a.Post_Uid
    FROM Post a JOIN Categories b ON a.Post_Category_ID = b.Category_ID JOIN Image c ON c.Imageid = a.Pid
    WHERE b.Category_ID IN (SELECT a.Post_Category_ID FROM POST a JOIN
-   Likebyuser b ON a.Pid = b.LikebyUser_Pid WHERE b.LikebyUser_Uid = 2)
+   Likebyuser b ON a.Pid = b.LikebyUser_Pid WHERE b.LikebyUser_Uid = 2) 
 
---  display recommended post like by your friend 
-SELECT a.Pid,a.Title,a.Description,c.Image,a.Likes,b.Category_Name,a.Post_Date,a.Post_Uid FROM Post a JOIN Categories b
+-- friends of friends like . display recommended post like by your friend 
+ with  friendss(Uid)
+  AS
+  (SELECT Uid FROM  Users WHERE  Uid  IN 
+(Select DISTINCT FriendRequest_Uid FROM  FriendRequest WHERE FriendRequest_Frid = 1 AND FriendStatus = 1)
+OR  Uid  IN (Select DISTINCT FriendRequest_Frid FROM 
+FriendRequest WHERE FriendRequest_Uid  = 1 AND FriendStatus = 1))
+,
+friendoffriend(Uid) AS 
+(SELECT Uid FROM  Users WHERE  Uid  IN 
+(Select DISTINCT FriendRequest_Uid FROM  FriendRequest WHERE FriendRequest_Frid IN (SELECT Uid FROM friendss)
+AND FriendStatus = 1 AND FriendRequest_Uid <> 1)
+OR  Uid  IN (Select DISTINCT FriendRequest_Frid FROM 
+FriendRequest WHERE FriendRequest_Uid  IN (SELECT Uid FROM friendss)
+AND FriendStatus = 1 AND FriendRequest_Frid <> 1))
+
+SELECT a.Pid,a.Title,a.Description,c.Image,a.Likes,b.Category_Name,a.Post_Date,a.Post_Uid
+FROM Post a JOIN Categories b
 ON a.Post_Category_ID = b.Category_ID JOIN Image c ON c.Imageid = a.Pid
-WHERE a.Pid IN(SELECT Pid FROM Likebyuser WHERE LikebyUser_Uid IN (SELECT Uid FROM  Users WHERE
-Uid  IN (Select DISTINCT Uid FROM  FriendRequest WHERE FriendRequest_Frid = 5  AND FriendStatus = 1)
-OR  Uid  IN (Select DISTINCT FriendRequest_Frid FROM  FriendRequest WHERE FriendRequest_Uid  = 5 AND FriendStatus = 1))) 
+WHERE
+a.Pid IN(SELECT Pid FROM Likebyuser WHERE LikebyUser_Uid IN (SELECT Uid FROM friendoffriend))
+
 --
---  acceapt request
+--  acceapt request .
 UPDATE FriendRequest SET FriendStatus = 1, Approved_Date = GETDATE() WHERE FriendRequest_Uid = 1 AND FriendRequest_Frid = 5
+
+-- request apporoved .
+SELECT FriendRequest_Frid  FROM FriendRequest WHERE FriendStatus = 1 AND FriendRequest_Uid = 1
+-- requested friend .
+SELECT FriendRequest_Frid  FROM FriendRequest WHERE FriendStatus = 0 AND FriendRequest_Uid = 0
+-- my all friends .
+SELECT Name,Uid FROM  Users WHERE  Uid  IN 
+(Select DISTINCT FriendRequest_Uid FROM  FriendRequest WHERE FriendRequest_Frid = 1 AND FriendStatus = 1)
+OR  Uid  IN (Select DISTINCT FriendRequest_Frid FROM
+FriendRequest WHERE FriendRequest_Uid  = 1 AND FriendStatus = 1)
+
+-- friends of friends .
+  with  friends(Uid)
+  AS
+  (SELECT Uid FROM  Users WHERE  Uid  IN 
+(Select DISTINCT FriendRequest_Uid FROM  FriendRequest WHERE FriendRequest_Frid = 1 AND FriendStatus = 1)
+OR  Uid  IN (Select DISTINCT FriendRequest_Frid FROM 
+FriendRequest WHERE FriendRequest_Uid  = 1 AND FriendStatus = 1))
+
+SELECT Uid FROM  Users WHERE  Uid  IN 
+(Select DISTINCT FriendRequest_Uid FROM  FriendRequest WHERE FriendRequest_Frid IN (SELECT Uid FROM friends)
+AND FriendStatus = 1 AND FriendRequest_Uid <> 1)
+OR  Uid  IN (Select DISTINCT FriendRequest_Frid FROM 
+FriendRequest WHERE FriendRequest_Uid  IN (SELECT Uid FROM friends)
+AND FriendStatus = 1 AND FriendRequest_Frid <> 1)
+
 
 INSERT INTO Groups VALUES
 ('grp1','this is our college group',2,'2021-01-01'),
